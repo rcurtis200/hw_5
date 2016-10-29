cf864a2f6158ab5ad29c342b262bd8fd0720fa95
Riley Curtis <rileycurtis@mail.weber.edu>
2016-10-29 10:41:11 -0600

    "added __name__ == 'main'"

--- hw5.py
+++ hw5.py
@@ -1,24 +1,31 @@
-#!/usr/var/env python3
+#!/usr/bin/env python3
+"""
+riley_curtis_hw5.py
+Asks the user to enter their PIN. If they enter it incorrectly three times the card is blocked.
+"""
 
-import sys
 
-strikes = 0
-
-while strikes != 3:
-    user_input = input("Enter your PIN: ")
-    if user_input == "1234":
-        print("Your PIN is correct")
-        exit(0)
-    else:
-        if len(user_input) != 4:
-            print("Invalid PIN length. Correct format is <9876> ")
-            strikes += 1
-        elif not user_input.isdigit():
-            print("Invalid PIN character(s). Correct format is <9876> ")
-            strikes += 1
+def main():
+    strikes = 0
+    while strikes != 3:
+        user_input = input("Enter your PIN: ")
+        if user_input == "1234":
+            print("Your PIN is correct")
+            exit(0)
         else:
-            print("Invalid PIN. ")
-            strikes += 1
+            if len(user_input) != 4:
+                print("Invalid PIN length. Correct format is <9876> ")
+                strikes += 1
+            elif not user_input.isdigit():
+                print("Invalid PIN character(s). Correct format is <9876> ")
+                strikes += 1
+            else:
+                print("Invalid PIN. ")
+                strikes += 1
+    print("Your card is blocked")
+    exit(1)
 
-print("Your card is blocked")
-exit(1)
+if __name__ == "__main__":
+    main()
+
+exit(0)
